df701158d99a9942ffc912370c90b99e5765b1b7
shyunc <shyunc.m@gmail.com>
2025-01-20 14:33:55 +0900

    MTL path updated

--- MTL/MTL_server.py
+++ MTL/MTL_server.py
@@ -7,6 +7,14 @@ import json
 from MTL.command import *
 
 
+
+def ensure_directory(dir_path):
+    if not os.path.exists(dir_path):
+        print(f"There is no directroy. Now making directory: {dir_path}")
+        os.makedirs(dir_path)
+    else:
+        print(f"There is alireay the directory. Skip making directory : {dir_path}")
+
 async def main():
     with open('./Lib/KSPEC.ini','r') as f:
         kspecinfo=json.load(f)
@@ -14,6 +22,11 @@ async def main():
     ip_addr = kspecinfo['RabbitMQ']['ip_addr']
     idname = kspecinfo['RabbitMQ']['idname']
     pwd = kspecinfo['RabbitMQ']['pwd']
+
+    data_path=kspecinfo['MTL']['mtlimagepath']
+    ensure_directory(data_path)
+    file_path=kspecinfo['MTL']['mtlfilepath']
+    ensure_directory(file_path)
 
     print('MTL Server Started!!!')
     MTL_server=AMQclass(ip_addr,idname,pwd,'MTL','ics.ex')
