ee31b3c6a7e4a05d124a299045e7a5b1a0cd3ca6
shyunc <shyunc.m@gmail.com>
2025-02-12 15:56:14 +0900

    TCS update

--- KSPECRUN.py
+++ KSPECRUN.py
@@ -1,6 +1,12 @@
-import os, sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#
+# @Author: Sang-Hyun Chun (shyunc@kasi.re.kr)
+# @Date: 2025-01-12
+# @Filename: KSPECRUN.py
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import os
+import sys
 import asyncio
 from Lib.AMQ import *
 from icscommand import *
@@ -8,20 +14,40 @@ import aio_pika
 import Lib.process as processes
 import json
 
-class kspecicsclass():
+class kspecicsclass:
+    """
+    Class to handle inter-process communication and command handling for KSPEC.
+
+    Attributes:
+        transport (str): The transport layer for communication using UDP.
+        tcslist (list): List of TCS commands for TCS communication.
+        running (bool): Flag to control the running state of the process.
+    """
+
     def __init__(self):
+        """
+        Initializes kspecicsclass with default attributes.
+        """
         self.transport = 'None'
-        self.tcslist = ["start","stop","tcsint","tcsreset","tcsclose",
-            "tcsarc","tcsstatus","tstat","traw","tsync","tcmd",
-            "treg","tmradec","tmr","tmobject","tmo","tmelaz",
-            "tme","tmoffset","toff","tstop","tstow","tdi",
-            "cc","oo","nstset","nston","nstoff","auxinit",
-            "auxreset","auxclose","auxarc","auxstatus",
-            "astat","acmd","fsastat","fs","fttstat",
-            "ft","dfocus","dtilt","fttgoto",]
+        self.tcslist = [
+            "start", "stop", "tcsint", "tcsreset", "tcsclose",
+            "tcsarc", "tcsstatus", "tstat", "traw", "tsync", "tcmd",
+            "treg", "tmradec", "tmr", "tmobject", "tmo", "tmelaz",
+            "tme", "tmoffset", "toff", "tstop", "tstow", "tdi",
+            "cc", "oo", "nstset", "nston", "nstoff", "auxinit",
+            "auxreset", "auxclose", "auxarc", "auxstatus",
+            "astat", "acmd", "fsastat", "fs", "fttstat",
+            "ft", "dfocus", "dtilt", "fttgoto"
+        ]
         self.running = True
 
-    async def response_act(self,ICS_client):
+    async def response_act(self, ICS_client):
+        """
+        Handles responses from the ICS client asynchronously.
+
+        Args:
+            ICS_client (AMQclass): The AMQ client instance for communication.
+        """
         try:
             while self.running:
                 rsp_msg = await ICS_client.receive_message('ICS')
@@ -29,33 +55,33 @@ class kspecicsclass():
                 inst = dict_data['inst']
                 process_status = dict_data['process']
                 print('process status :', process_status)
-                processes.update_process(inst,process_status)
+                processes.update_process(inst, process_status)
 
                 saveflag = dict_data["savedata"]
                 if saveflag == "False":
-                    print('\033[94m'+'[ICS] received: ',dict_data["message"]+'\n\033[0m')
+                    print('\033[94m' + '[ICS] received: ', dict_data["message"] + '\n\033[0m')
                 else:
-                    with open('./Lib/KSPEC.ini','r') as fs:
-                        kspecinfo=json.load(f)
+                    with open('./Lib/KSPEC.ini', 'r') as fs:
+                        kspecinfo = json.load(fs)
 
-                    savefilepath=kspecinfo['savepath']
+                    savefilepath = kspecinfo['savepath']
 
                     with open(savefilepath + dict_data["filename"], "w") as f:
                         json.dump(dict_data, f)
-                    outfile=dict_data["filename"]
-                    print('\033[94m'+'\n[ICS] received: ',dict_data["message"]+'\033[0m')
-                    print('\033[94m'+f'[ICS] "{outfile}" File saved'+'\033[0m')
+                    outfile = dict_data["filename"]
+                    print('\033[94m' + '\n[ICS] received: ', dict_data["message"] + '\033[0m')
+                    print('\033[94m' + f'[ICS] "{outfile}" File saved' + '\033[0m')
 
                 nextstep = dict_data['nextstep']
                 if nextstep == 'True':
-                    message=dict_data['nextstep']
-                    cmd=message.split(" ")
+                    message = dict_data['nextstep']
+                    cmd = message.split(" ")
 
                     if cmd[0] in self.tcslist:
                         self.transport.sendto(message.encode())  # Send message to server
 
                     else:
-                        await identify(message,ICS_client,self.transport)
+                        await identify(message, ICS_client, self.transport)
 
                 if nextstep == 'None':
                     pass
@@ -66,43 +92,43 @@ class kspecicsclass():
         finally:
             print("response_act finalized.")
 
+    async def user_input(self, ICSclient):
+        """
+        Handles user input asynchronously and sends commands to the server.
 
-    async def user_input(self,ICSclient):
+        Args:
+            ICSclient (AMQclass): The AMQ client instance for communication.
+        """
         try:
             loop = asyncio.get_event_loop()
             on_con_lost = loop.create_future()  # Future to wait for connection loss
 
-    # Define a factory function that returns a new protocol instance
+            # Define a factory function that returns a new protocol instance
             def protocol_factory():
                 return UDPClientProtocol(on_con_lost)
 
-    # Create UDP connection
+            # Create UDP connection
             self.transport, self.protocol = await loop.create_datagram_endpoint(
                 protocol_factory, remote_addr=("127.0.0.1", 12345)
             )
 
-
-    #    try:
             while True:
-        # Run input() in the default executor (blocking call to non-blocking)
+                # Run input() in the default executor (blocking call to non-blocking)
                 message = await loop.run_in_executor(None, input, "\n Input command: ")
                 cmd = message.split(" ")
+                messagetcs='KSPEC>TC '+message
 
                 if cmd[0] in self.tcslist:
-                    self.transport.sendto(message.encode())  # Send message to server
+                    self.transport.sendto(messagetcs.encode())  # Send message to server
 
                 elif message.lower() == "quit":  # Exit condition
                     print("Closing connection...")
                     self.transport.close()  # Close the connection
-                    print(type(ICSclient))
-#                    await ICSclient.disconnect()
                     self.running = False
-#                on_con_lost.set_result(None)  # Wait until the connection is closed
-#                asyncio.get_running_loop().stop()
                     break
 
                 else:
-                    await identify(message,ICSclient,self.transport)
+                    await identify(message, ICSclient, self.transport)
         except asyncio.CancelledError:
             print("user_input cancelled.")
         except Exception as e:
@@ -111,9 +137,11 @@ class kspecicsclass():
             print("user_input finalized.")
 
 async def main():
-
-    with open('./Lib/KSPEC.ini','r') as f:
-        kspecinfo=json.load(f)
+    """
+    Main function to initialize and run the asynchronous tasks.
+    """
+    with open('./Lib/KSPEC.ini', 'r') as f:
+        kspecinfo = json.load(f)
 
     ip_addr = kspecinfo['RabbitMQ']['ip_addr']
     idname = kspecinfo['RabbitMQ']['idname']
@@ -123,10 +151,10 @@ async def main():
     await ICS_client.connect()
     await ICS_client.define_producer()
     await ICS_client.define_consumer()
-    kspecics=kspecicsclass()
-    
+    kspecics = kspecicsclass()
+
     try:
-        await asyncio.gather(kspecics.user_input(ICS_client),kspecics.response_act(ICS_client))
+        await asyncio.gather(kspecics.user_input(ICS_client), kspecics.response_act(ICS_client))
     except asyncio.CancelledError:
         print("Main coroutine cancelled.")
     except Exception as e:
@@ -137,8 +165,10 @@ async def main():
             await ICS_client.disconnect()
             print("Main finalized")
 
-
 if __name__ == "__main__":
+    """
+    Entry point for the script. Runs different modules based on the input argument.
+    """
     if sys.argv[1] == 'ics':
         try:
             asyncio.run(main())  # Run the main coroutine
@@ -178,6 +208,4 @@ if __name__ == "__main__":
     if sys.argv[1] == 'ADCsimul':
         from ADC.Simul import ADC_server
         asyncio.run(ADC_server.main())
-
-
 
